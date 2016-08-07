d4ab5ee63f9e5312190977d7fb53f9c577640309
Sophie Jiang <sifeij@gmail.com>
2016-08-07 11:11:11 -0700

    WIP - set up unit tests and e2e tests

---
+++ src/ui/e2e/app.e2e-spec.ts
@@ -0,0 +1,13 @@
+// describe('QuickStart E2E Tests', function () {
+
+//     let expectedMsg = 'My First Angular 2 App';
+
+//     beforeEach(function () {
+//         browser.get('');
+//     });
+
+//     it('should display: ' + expectedMsg, function () {
+//         expect(element(moveBy.css('h1')).getText()).toEqual(expectedMsg);
+//     });
+    
+// })
