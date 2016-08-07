ce7ca4ed3a6f6cca7c3038ff8126d8ab360a2fc8
Sophie Jiang <sifeij@gmail.com>
2016-08-07 12:21:41 -0700

    Use templateUrl and implements OnInit interface

--- src/ui/app/app.component.ts
+++ src/ui/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CORE_DIRECTIVES } from '@angular/common';
 
 import { Product } from './models/product';
@@ -6,30 +6,17 @@ import { BalanceService } from './services/balance-service';
 
 @Component({
   selector: 'exchange-app',
-  template: `
-    <nav class="navbar navbar-default navbar-inverse">
-      <a class="navbar-brand" href="#">Exchange</a>
-    </nav>
-    <div class="container-fluid">
-      <div class="row">
-        <table class="col-xs-10 col-xs-offset-1">
-          <tr *ngFor="let product of products">
-            <td>{{ product.name }}</td><td class="pull-right">{{ product.amount }}</td>
-          </tr>
-        </table>
-      </div>
-    </div>
-  `,
+  templateUrl: 'app/app.component.html',
   providers: [ BalanceService ],
   directives: [ CORE_DIRECTIVES ]
 })
-export class AppComponent {
-  products: Product[]; 
+export class AppComponent implements OnInit {
+  products: Product[];
   constructor(private service: BalanceService) {
 
   }
 
-  ngOnInit() {
+  public ngOnInit(): void {
      this.service
          .getBalance()
          .subscribe(products => this.products = products);
