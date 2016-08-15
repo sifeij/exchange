2245ce010636c59bb402de8878c03ff01a1d074e
sifei jiang <sifeij@gmail.com>
2016-08-15 06:31:07 -0700

    Demonstrate using promise instead of observable for http call

--- src/ui/app/main.ts
+++ src/ui/app/main.ts
@@ -6,6 +6,7 @@ import { HTTP_PROVIDERS } from '@angular/http';
 import { AppComponent }       from './components/app.component';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/toPromise';
 
 let appProviders = [
     HTTP_PROVIDERS
--- src/ui/app/services/transaction-service.ts
+++ src/ui/app/services/transaction-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {
     Http,
     Headers,
+    Response,
     RequestOptions
 } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
@@ -14,38 +15,52 @@ export class TransactionService {
 
     private transactionUrl = `${BASE_URI}/api/transaction`;
 
-    constructor(private _http: Http) {}
+    constructor(private _http: Http) { }
 
     get(): Observable<Transaction[]> {
         return this._http
-                   .get(this.transactionUrl)
-                   .map(t => t.json())
-                   .catch(this.handleError);
+            .get(this.transactionUrl)
+            .map(t => t.json())
+            .catch(this.handleError);
     }
 
     getSummary(): Observable<Transaction[]> {
         let url = `${this.transactionUrl}/summary`;
         return this._http
-                   .get(url)
-                   .map(t => t.json())
-                   .catch(this.handleError);
+            .get(url)
+            .map(t => t.json())
+            .catch(this.handleError);
     }
 
-    getByCurrency(currency: string): Observable<Transaction> {
+    // try out using promise
+    getByCurrency(currency: string): Promise<Transaction[]> {
         let url = `${this.transactionUrl}/{transaction.currency}`;
         return this._http
-                   .get(url)
-                   .map(t => t.json)
-                   .catch(this.handleError);
+            .get(url)
+            .map((response: Response) => response.json())
+            .toPromise()
+            .catch((err: any) => {
+                console.log(err);
+                return Promise.reject(err);
+            });
     }
+
+    /* To use promise in component
+    public transaction: Transaction;
+    this._transactionService.getbyCurrency('USD')
+        .then((trans) => this.transaction = trans)
+        .catch((err) => {
+            console.log(err);
+        })
+    */
 
     deposit(currency: string, amount: number): Observable<any> {
         let url = `${this.transactionUrl}/${currency}`;
         let headers = new Headers({ 'Content-Type': 'application/json' });
         let options = new RequestOptions({ headers: headers });
         return this._http
-                   .put(url, amount, options)
-                   .catch(this.handleError);
+            .put(url, amount, options)
+            .catch(this.handleError);
     }
 
     private handleError(error: any, caught: any) {
